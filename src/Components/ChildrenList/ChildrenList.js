e36d64c866faa5a10414977e704a2f10ab26410c
tal <tal.bny3@gmail.com>
2020-05-23 22:19:21 +0300

    beautify a bit

--- src/Components/ChildrenList/ChildrenList.js
+++ src/Components/ChildrenList/ChildrenList.js
@@ -3,10 +3,27 @@ import './ChildrenList.css';
 
 
 function ChildrenList() {
+
+    const list = ['tal', 'inon', 'mgr_ido'];
+
+    const getChildRow = (childName) => {
+        return (
+            <div className="child-row">
+                <p style={{margin:0, marginLeft: 10, fontSize:20}}>{childName}</p>
+            </div>
+        )
+    }
+
     return (
       <div className="children-card">
-        <i id="goBackBtn" className="back-button fas fa-undo-alt"></i>
-      </div>
+          <div className="path-container" style={{paddingLeft: 10}}>
+            <i id="goBackBtn" style={{float:'left', marginRight:10}} className="back-button fas fa-undo-alt"></i>
+            <p> /aa/bbb/ccc</p>
+          </div>
+        
+        { list.map(el => getChildRow(el))
+        }
+        </div>
     );
   }
   
