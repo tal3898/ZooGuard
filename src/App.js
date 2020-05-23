465799f41c289f01dbcdcfec29fec82ca0e030d5
tal <tal.bny3@gmail.com>
2020-05-23 21:07:36 +0300

    added font awesome

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar'
+import ChildrenList from './Components/ChildrenList/ChildrenList'
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
 
+      <ChildrenList/>
     </div>
   );
 }
---
+++ src/Components/ChildrenList/ChildrenList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ChildrenList.css';
+
+
+function ChildrenList() {
+    return (
+      <div className="children-card">
+        <i id="goBackBtn" className="back-button fas fa-undo-alt"></i>
+      </div>
+    );
+  }
+  
+  export default ChildrenList;
+  
