258e05eae3d091e0073164dc16d8c0f246edb636
tal <tal.bny3@gmail.com>
2020-05-23 20:50:30 +0300

    added basic navbar

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './NavBar.css';
+
+
+function NavBar() {
+    return (
+      <div className="navbar-div">
+        <img src="zoo.png" style={{height:'100%'}} />
+          <span style={{fontSize:60}}>ZooGuard</span>
+      </div>
+    );
+  }
+  
+  export default NavBar;
+  
