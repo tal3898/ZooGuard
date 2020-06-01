099081ab643af25a01b766f868b16726a6927a8a
tal <tal.bny3@gmail.com>
2020-06-01 21:10:58 +0300

    now can read the client post body

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
-
+var bodyParser = require('body-parser')
 const app = express();
 
-const port = 5000;
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+	extended: true
+}));
+
+const port = 5000;  
 
 app.post('/nodeData', async (req, res) => {
 
+    var path = req.body.path;
+    console.log(path);
+    
 	res.json({
         children: ['r','t','y'],
         data: {
