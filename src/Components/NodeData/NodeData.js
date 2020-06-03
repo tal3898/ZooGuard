b3b4a99ecd1d5b61f0396ad9cc271f2aa12a0539
tal <tal.bny3@gmail.com>
2020-06-03 12:26:26 +0300

    added popup that when clicking on delete, the popup jumps

--- src/Components/NodeData/NodeData.js
+++ src/Components/NodeData/NodeData.js
@@ -2,20 +2,43 @@ import React from 'react';
 import './NodeData.css';
 import Button from '@material-ui/core/Button';
 
+import Popup from "reactjs-popup";
+
+
 function NodeData(props) {
+    const [isDeletePopupOpen, setIsDeletePopupOpen] = React.useState(false);
+
 
     return (
-        <div style={{}} className="data-container">
-            <Button style={{position:'absolute', right:20}} variant="contained" color="primary">
-                Add
-            </Button>
-            <Button style={{position:'absolute', right:70}} variant="contained" color="secondary">
-                Delete
-            </Button>
-            <p>data: {props.nodeData}</p>
-            <p>creation time (ctime): {props.creationTime}</p>
-            <p>modification time (mtime): {props.modificationTime}</p>
-            <p>Number of children: {props.numberOfChildren}</p>
+        <div>
+            {/** Delete node popup */}
+            <Popup
+                open={isDeletePopupOpen}
+                onClose={() => setIsDeletePopupOpen(false)}
+                modal
+                active
+                closeOnDocumentClick
+            >
+                <center style={{padding:15}}>
+                    <p>Are you sure you want to delete node?</p>
+                    <Button variant="contained" color="primary">
+                        בוודאי
+                    </Button>
+                </center>
+            </Popup>
+
+            <div style={{}} className="data-container">
+                <Button style={{ position: 'absolute', right: 20 }} variant="contained" color="primary">
+                    Add
+                </Button>
+                <Button style={{ position: 'absolute', right: 70 }} onClick={() => setIsDeletePopupOpen(true)} variant="contained" color="secondary">
+                    Delete
+                </Button>
+                <p>data: {props.nodeData}</p>
+                <p>creation time (ctime): {props.creationTime}</p>
+                <p>modification time (mtime): {props.modificationTime}</p>
+                <p>Number of children: {props.numberOfChildren}</p>
+            </div>
         </div>
     );
 }
